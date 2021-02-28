4b26450f8826f7dcc5560c832cb27541bbfd33ba
watamario15 <watamario15@outlook.jp>
2021-02-28 15:13:27 +0900

    Add termux

--- termux/initialize.sh
+++ termux/initialize.sh
@@ -12,32 +12,20 @@ if [ $# -eq 1 ]; then
             unzip casl2-2017-11-16.zip
             cd casl2-2017-11-16
             chmod +x casl2 comet2
-            sudo mv casl2 comet2 /usr/local/bin/
+            mv casl2 comet2 /data/data/com.termux/files/usr/local/bin/
             cd ..
             rm -rf casl2-2017-11-16.zip casl2-2017-11-16
             echo "The Installation Completed."
         fi
-    elif [ $1 = "brain" ]; then
-        if !(grep -Fq "deb [trusted=yes] https://max.kellermann.name/debian cegcc_buster-default main" "/etc/apt/sources.list"); then
-            echo "Registering the repository of CeGCC..."
-            sudo bash -c 'echo >> /etc/apt/sources.list'
-            sudo bash -c 'echo "deb [trusted=yes] https://max.kellermann.name/debian cegcc_buster-default main" >> /etc/apt/sources.list'
-        fi
-        sudo cp tools/CeGCC /usr/local/bin/
-        sudo chmod +x /usr/local/bin/CeGCC
-        echo "Installing packages for SHARP Brain developments..."
-        sudo apt update
-        sudo apt install gcc-arm-mingw32ce gcc-arm-linux-gnueabi bison flex libncurses5-dev libssl-dev debootstrap qemu-user-static
-        echo "The Installation Completed."
     else
         echo "No such option: $1"
     fi
 elif [ $# -eq 0 ]; then
     echo "Installing essential packages..."
-    sudo apt update
-    sudo apt install curl wget zip unzip gawk vim build-essential exfat-fuse exfat-utils xsel peco
-    sudo curl -fL https://raw.githubusercontent.com/puhitaku/rcs/master/scripts/fontify -o /usr/local/bin/fontify
-    sudo chmod +x /usr/local/bin/fontify
+    apt update
+    apt install curl wget zip unzip gawk vim build-essential
+    curl -fL https://raw.githubusercontent.com/puhitaku/rcs/master/scripts/fontify -o /data/data/com.termux/files/usr/local/bin/fontify
+    chmod +x /data/data/com.termux/files/usr/local/bin/fontify
     echo "Setting up the Git..."
     echo -n "Your email address: "; read email
     git config --global user.email ${email}
---
+++ termux/install.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# 未定義な変数があったら途中で終了する
+set -u
+
+# 2 文字以上の dotfiles に対して
+for f in .??*; do
+    [ "$f" = ".git" ] && continue
+    [ "$f" = ".gitconfig.local.template" ] && continue
+    [ "$f" = ".gitmodules" ] && continue
+
+    # Symlink を貼る
+    ln -snfv ${PWD}/"$f" ~/
+done
