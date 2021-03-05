a9be6fbbbed1e4cf43944cd3f7bca2af33f1f324
watamario15 <watamario15@outlook.jp>
2021-03-05 15:09:07 +0900

    Fix

--- initialize.sh
+++ initialize.sh
@@ -33,7 +33,7 @@ if [ $# -eq 1 ]; then
         sudo apt install kakasi libkakasi2-dev libxml2-dev liblzma-dev
 
         cd
-        wget https://drive.google.com/uc?export=download&id=0B4y35FiV1wh7cENtOXlicTFaRUE
+        wget "https://drive.google.com/uc?export=download&id=0B4y35FiV1wh7cENtOXlicTFaRUE" -O mecab-0.996.tar.gz
         tar zxvf mecab-0.996.tar.gz
         cd mecab-0.996
         ./configure --with-charset=utf8 --enable-utf8-only
@@ -41,7 +41,7 @@ if [ $# -eq 1 ]; then
         sudo make install
 
         cd
-        wget https://drive.google.com/uc?export=download&id=0B4y35FiV1wh7MWVlSDBCSXZMTXM
+        wget "https://drive.google.com/uc?export=download&id=0B4y35FiV1wh7MWVlSDBCSXZMTXM" -O mecab-ipadic-2.7.0-20070801.tar.gz
         tar zxvf mecab-ipadic-2.7.0-20070801.tar.gz
         cd mecab-ipadic-2.7.0-20070801
         ./configure --with-charset=utf8
@@ -49,13 +49,13 @@ if [ $# -eq 1 ]; then
         sudo make install
 
         cd
-        git clone --depth 1 https://github.com/neologd/mecab-ipadic-neologd.git
+        git clone --depth 1 "https://github.com/neologd/mecab-ipadic-neologd.git"
         cd mecab-ipadic-neologd
         ./bin/install-mecab-ipadic-neologd -n -a
         sudo nano -w $(mecab-config --sysconfdir)/mecabrc # 辞書ファイルを指定している箇所を編集し、標準で使用したい辞書を指定
 
         cd
-        wget https://github.com/yvt/xtbook/releases/download/v0.2.6/MkXTBWikiplexus-R3.tar.gz
+        wget "https://github.com/yvt/xtbook/releases/download/v0.2.6/MkXTBWikiplexus-R3.tar.gz"
         tar zxvf MkXTBWikiplexus-R3.tar.gz
         cd MkXTBWikiplexus/build.unix
         sudo nano -w ../MkImageComplex/main.cpp # MkImageComplex フォルダの main.cpp の gets(buf) を scanf("%s",buf)!=EOF に変える。
