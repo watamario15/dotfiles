8883213437501a5eba43a88662117075ba621836
watamario15 <watamario15@outlook.jp>
2021-03-23 16:36:01 +0900

    Further fix

--- brainux/initialize.sh
+++ brainux/initialize.sh
@@ -3,9 +3,11 @@
 # Manually expand the system partition before running this script!
 
 set -e
-echo "Enter the SD path: "; read sd
+echo -n "Enter the SD path: "; read sd
 sudo cp $(which casl2) $(which comet2) $(which fontify) ${sd}/usr/local/bin/
-sudo cp .bashrc .nanorc .vimrc .fbtermrc .uim swap.sh usbg.sh ${sd}/root
+cp .bashrc .nanorc .vimrc .fbtermrc .uim swap.sh usbg.sh ${sd}/home/user/
+sudo dd if=/dev/zero of=${sd}/swapfile bs=1M count=1024
+sudo chmod 0600 /swapfile
 sudo chroot $sd
 
 # Manually run:
--- brainux/swap.sh
+++ brainux/swap.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-dd if=/dev/zero of=/swapfile bs=1M count="${1:-1024}"
+if(! test -e "/swapfile"); then
+    dd if=/dev/zero of=/swapfile bs=1M count="${1:-1024}"
+fi
 chmod 0600 /swapfile
 mkswap /swapfile
 swapon /swapfile
