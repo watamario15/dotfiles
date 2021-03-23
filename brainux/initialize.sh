832d95a5751867ff039bcdf6a402f1734d049b6f
watamario15 <watamario15@outlook.jp>
2021-03-23 14:55:10 +0900

    Update initialize.sh to fit for Brainux 2021-03-22-233911

--- brainux/initialize.sh
+++ brainux/initialize.sh
@@ -9,5 +9,4 @@ sudo cp .bashrc .nanorc .vimrc .fbtermrc .uim swap.sh usbg.sh ${sd}/root
 sudo chroot $sd
 
 # Manually run:
-# apt install zip unzip gawk exfat-fuse exfat-utils xsel peco fbterm uim-fep uim-anthy
-# echo -e 'Section "Device"\n        Identifier "device"\n        Driver     "fbdev"\nEndSection\nSection "Screen"\n        Identifier "screen"\n        Device     "device"\nEndSection' > /etc/X11/xorg.conf
+# apt install zip unzip gawk exfat-fuse exfat-utils xsel peco
