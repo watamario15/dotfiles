27fe103f808ebbea994c9cc69efab00dbfc0e0f4
watamario15 <watamario15@outlook.jp>
2021-04-19 22:12:35 +0900

    Update files for Brainux

--- brainux/initialize.sh
+++ brainux/initialize.sh
@@ -4,11 +4,11 @@
 
 set -e
 echo -n "Enter the SD path: "; read sd
-sudo cp $(which casl2) $(which comet2) $(which fontify) ${sd}/usr/local/bin/
-cp .bashrc .nanorc .vimrc .fbtermrc .uim swap.sh usbg.sh ${sd}/home/user/
+sudo cp $(which casl2) $(which comet2) $(which fontify) swap.sh usbg.sh ${sd}/usr/local/bin/
+cp .bashrc .nanorc .vimrc .fbtermrc .uim ${sd}/home/user/
 sudo dd if=/dev/zero of=${sd}/swapfile bs=1M count=1024
-sudo chmod 0600 /swapfile
+sudo chmod 0600 ${sd}/swapfile
 sudo chroot $sd
 
 # Manually run:
-# apt install git-lfs zip unzip gawk exfat-fuse exfat-utils xsel peco
+# apt install zip unzip xsel peco
--- brainux/swap.sh
+++ brainux/swap.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 if(! test -e "/swapfile"); then
-    dd if=/dev/zero of=/swapfile bs=1M count="${1:-1024}"
+    sudo dd if=/dev/zero of=/swapfile bs=1M count="${1:-1024}"
 fi
-chmod 0600 /swapfile
-mkswap /swapfile
-swapon /swapfile
+sudo chmod 0600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
