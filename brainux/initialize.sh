3601632ebc97879c9daa3cc0f88ffec88a0af775
watamario15 <watamario15@outlook.jp>
2021-06-08 12:16:19 +0900

    Update .bashrc and the initializer of Brainux

--- brainux/initialize.sh
+++ brainux/initialize.sh
@@ -10,5 +10,6 @@ sudo chmod 0600 ${sd}/swapfile
 sudo mkswap ${sd}/swapfile
 sudo chroot $sd << EOF
 apt install -y zip unzip xsel peco
-echo "/swapfile  swap   swap    defaults   0 0" >> /etc/fstab
+echo "/swapfile  none   swap    defaults   0 0" >> /etc/fstab
+sed -ie "s%PATH=\"/usr/local/bin:/usr/bin:/bin:/usr/games:/usr/local/games\"%PATH=\"/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games\"%" /etc/profile
 EOF
