d6a082f98190660bac46b8a49a71dba3b6d4c891
watamario15 <watamario15@outlook.jp>
2022-03-09 16:52:17 +0900

    Update XTBook conversion assistant

--- initialize.sh
+++ initialize.sh
@@ -15,7 +15,7 @@ if [ $# -eq 1 ]; then
             sudo mv casl2 comet2 /usr/local/bin/
             cd ..
             rm -rf casl2-2017-11-16.zip casl2-2017-11-16
-            echo "The Installation Completed."
+            echo "Done."
         fi
 
     elif [ $1 = "brain" ]; then
@@ -29,15 +29,15 @@ if [ $# -eq 1 ]; then
         echo "Installing packages for SHARP Brain developments..."
         sudo apt update
         sudo apt install -y gcc-arm-mingw32ce gcc-arm-linux-gnueabi bison flex libncurses5-dev libssl-dev debootstrap qemu-user-static
-        echo "The Installation Completed."
+        echo "Done."
 
     elif [ $1 = "xtbook" ]; then
         echo "Installing packages..."
-        sudo apt install -y mecab libmecab-dev mecab-ipadic-utf8 kakasi libkakasi2-dev libxml2-dev liblzma-dev
+        sudo apt install -y mecab libmecab-dev mecab-ipadic-utf8 kakasi libkakasi2-dev libxml2-dev liblzma-dev imagemagick
 
         echo "Setting up libiconv..."
         cd /usr/src
-        sudo wget https://ftp.gnu.org/gnu/libiconv/libiconv-1.14.tar.gz -O - | sudo tar zxvf -
+        curl -sfL https://ftp.gnu.org/gnu/libiconv/libiconv-1.14.tar.gz | sudo tar zxvf -
         cd libiconv-1.14
         sudo sed -i -e 's%_GL_WARN_ON_USE (gets%//_GL_WARN_ON_USE (gets%' srclib/stdio.in.h
         sudo ./configure
@@ -49,10 +49,23 @@ if [ $# -eq 1 ]; then
         echo "Setting up MkXTBWikiplexus..."
         cd
         sudo rm -rf /usr/src/libiconv-1.14
-        wget https://github.com/yvt/xtbook/releases/download/v0.2.6/MkXTBWikiplexus-R3.tar.gz -O - | tar zxvf -
+        curl -fL https://github.com/yvt/xtbook/releases/download/v0.2.6/MkXTBWikiplexus-R3.tar.gz | tar zxvf -
         sudo sed -i -e 's/gets(buf)/scanf("%s",buf)!=EOF/' MkXTBWikiplexus/MkImageComplex/main.cpp
         cd MkXTBWikiplexus/build.unix
         make
+
+        cd `dirname $0` # Come back to the dotfiles directory
+        sudo cp tools/XTBook /usr/local/bin/
+        sudo chmod +x /usr/local/bin/XTBook
+        while true; do
+            echo -n "Your dictionary path: "; read dict
+            echo -n "Correct (${dict}) [Y/n]? "; read key
+            if [ "$key" != "n" ]; then
+                break
+            fi
+        done
+        sudo sed -i -e "s%^DICTDIR=.*%DICTDIR=${dict}%" -e "s%^PLIST=.*%PLIST=$(cd `dirname $0`; pwd)/tools/info-plists%" /usr/local/bin/XTBook
+        echo "Done."
 
     else
         echo "No such option: $1"
@@ -61,13 +74,20 @@ if [ $# -eq 1 ]; then
 elif [ $# -eq 0 ]; then
     echo "Installing essential packages..."
     sudo apt update
-    sudo apt install -y git-lfs curl wget zip unzip gawk vim build-essential gdb mingw-w64 exfat-fuse exfat-utils xsel peco
+    sudo apt install -y git-lfs curl wget zip unzip bzip2 gawk vim build-essential gdb mingw-w64 exfat-fuse exfat-utils xsel peco
+    curl -sfL https://www.7-zip.org/a/7z2107-linux-x64.tar.xz | sudo tar Jxfp - -C /usr/local/bin
     sudo curl -fL https://raw.githubusercontent.com/puhitaku/rcs/master/scripts/fontify -o /usr/local/bin/fontify
     sudo chmod +x /usr/local/bin/fontify
     echo "Setting up Git..."
-    echo -n "Your email address: "; read email
+    while true; do
+        echo -n "Your email address: "; read email
+        echo -n "Your name: "; read name
+        echo -n "Correct (email: ${dict}, name: ${name}) [Y/n]? "; read key
+        if [ "$key" != "n" ]; then
+            break
+        fi
+    done
     git config --global user.email ${email}
-    echo -n "Your name: "; read name
     git config --global user.name ${name}
     git config --global color.ui true
     git config --global core.quotepath false
