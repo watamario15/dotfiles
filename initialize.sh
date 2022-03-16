415e4a034b593493d03332cddec485c47c754c1a
watamario15 <65015796+watamario15@users.noreply.github.com>
2022-03-16 23:05:46 +0900

    Supress unzip messages on initialize.sh caslcomet

--- initialize.sh
+++ initialize.sh
@@ -10,7 +10,7 @@ if [ $# -eq 1 ]; then
             echo "Installing the CASLII/COMETII simulator (Osaka University)..."
             cd
             curl -fLO https://www-hasegawa.ist.osaka-u.ac.jp/~ykoizumi/lecture/fco/files/casl2-2017-11-16.zip
-            unzip casl2-2017-11-16.zip
+            unzip -oq casl2-2017-11-16.zip
             cd casl2-2017-11-16
             chmod +x casl2 comet2
             sudo mv casl2 comet2 /usr/local/bin/
