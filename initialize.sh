cec287b4530d6203e03008e4fc138ba180100ab5
watamario15 <watamario15@outlook.jp>
2022-07-19 17:02:11 +0900

    Update 7-zip

--- initialize.sh
+++ initialize.sh
@@ -76,7 +76,7 @@ elif [ $# -eq 0 ]; then
     echo "Installing essential packages..."
     sudo apt update
     sudo apt install -y git-lfs curl wget zip unzip bzip2 gawk vim build-essential gdb mingw-w64 exfat-fuse exfat-utils xsel peco
-    curl -sfL https://www.7-zip.org/a/7z2200-linux-x64.tar.xz | sudo tar Jxfp - -C /usr/local/bin
+    curl -sfL https://www.7-zip.org/a/7z2201-linux-x64.tar.xz | sudo tar Jxfp - -C /usr/local/bin
     sudo curl -fL https://raw.githubusercontent.com/puhitaku/rcs/master/scripts/fontify -o /usr/local/bin/fontify
     sudo chmod +x /usr/local/bin/fontify
     echo "Setting up Git..."
