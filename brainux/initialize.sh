75a444c14a14a769dbf8817ecd79f0799eedfe1c
watamario15 <watamario15@outlook.jp>
2022-12-22 16:47:45 +0900

    Update the initialization script for Brainux

--- brainux/initialize.sh
+++ brainux/initialize.sh
@@ -2,16 +2,29 @@
 set -e
 
 echo "Make sure that you've manually expanded the rootfs partition before running this script!"
-echo -n "Enter the SD path (without the last slash): "; read sd
+
+echo "Remounting the SD with dev enabled..."
+echo "rootfs devices:"
+mount | grep rootfs
+echo -n "Enter the SD rootfs device (ex. sdb2): "; read sd
+sudo mount -o remount,dev /dev/${sd}
+mount | grep rootfs
+
+echo -n "Enter the SD rootfs full path (without the last slash): "; read sd
+
+echo "Copying files..."
 sudo cp $(which casl2) $(which comet2) $(which fontify) swap usbg ${sd}/usr/local/bin/
 cp ../.bashrc ../.nanorc .vimrc .fbtermrc .uim ${sd}/home/user/
+
+echo "Creating a swap file..."
 sudo dd if=/dev/zero of=${sd}/swapfile bs=1M count=4096
 sudo chmod 0600 ${sd}/swapfile
 sudo mkswap ${sd}/swapfile
+
+echo "chroot-ing into the SD rootfs to complete the initialization..."
 sudo chroot $sd << EOF
 apt update
 apt dist-upgrade -y
 apt install gdb zip unzip xsel peco -y
 echo "/swapfile  none   swap    defaults   0 0" >> /etc/fstab
-sed -ie "s%PATH=\"/usr/local/bin:/usr/bin:/bin:/usr/games:/usr/local/games\"%PATH=\"/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games\"%" /etc/profile
 EOF
