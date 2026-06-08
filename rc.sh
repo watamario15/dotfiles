cbe3e87fe0ed31784d23c45824a39f67adf276d1
watamario15 <otwthutu15@gmail.com>
2026-06-08 17:32:40 +0900

    Add zsh support

--- rc.sh
+++ rc.sh
@@ -1,28 +1,24 @@
 export BASH_SILENCE_DEPRECATION_WARNING=1
-HISTCONTROL=ignoreboth
-HISTSIZE=10000
-HISTFILESIZE=$HISTSIZE
-shopt -s histappend
-shopt -s checkwinsize
 
 get_status_color() {
   if [ "$1" -eq 0 ]; then
-    echo -e "\033[01;36m"
+    printf '\033[01;36m'
   else
-    echo -e "\033[01;31m"
+    printf '\033[01;31m'
   fi
   return "$1"  # Preserve the exit code
 }
 
 prompt_pwd() {
+  local cwd
   case "$PWD" in
-    ${HOME}*) cwd=~${PWD:${#HOME}};;
-    *) cwd=$PWD;;
+    ${HOME}*) cwd="~${PWD:${#HOME}}";;
+    *) cwd="$PWD";;
   esac
 
-  declare i=$(( ${#cwd} - 1 ))
-  declare cnt=0
-  declare result
+  local i=$(( ${#cwd} - 1 ))
+  local cnt=0
+  local result
 
   while true; do
     if [ $i -lt 0 ]; then
@@ -36,7 +32,7 @@ prompt_pwd() {
       if [ $cnt -ge 3 ]; then  # Tail directories count
         if [ $i -gt 0 ]; then
           result=${cwd:$i}
-          break;
+          break
         fi
       fi
     fi
@@ -44,7 +40,7 @@ prompt_pwd() {
     (( --i ))
   done
 
-  declare j=0
+  local j=0
   cnt=0
 
   while true; do
@@ -58,7 +54,7 @@ prompt_pwd() {
 
       if [ $cnt -ge 3 ]; then  # Head directories count
         if [ $j -lt $i ]; then
-          echo "${cwd:0:(($j + 1))}...$result"
+          echo "${cwd:0:$((j + 1))}...$result"
           return
         fi
       fi
@@ -67,11 +63,8 @@ prompt_pwd() {
     (( ++j ))
   done
 }
-
-PS1='\n\[$(get_status_color $?)\]$?\[\033[00m\] \[\033[01;32m\]\h\[\033[00m\]:\[\033[01;34m\]$(prompt_pwd)\[\033[00m\]\$ '
-
 # enable color support of ls and also add handy aliases
-if [ "$(uname)" = "Darwin" ]; then
+if [[ "$OSTYPE" == darwin* ]]; then
   colorize="-G"
 else
   colorize="--color=auto"
@@ -93,7 +86,6 @@ alias la='ls -A'
 alias l='ls -CF'
 
 alias editrc='nano -w ~/rc.sh'
-alias applyrc='. ~/.bashrc'
 
 alias pwdc='pwd | tr -d "\n" | xsel -ib'
 alias copy='xsel -ip && xsel -op | xsel -ib'
@@ -117,8 +109,8 @@ alias gm='git merge'
 alias gt='git log --graph --all --format="%as %C(cyan bold)%an%Creset %C(yellow)%h%Creset %C(green reverse)%d%Creset %s"'
 
 gps() {
-  declare args=()
-  declare isForcePush=0
+  local args=()
+  local isForcePush=0
 
   while (( $# > 0 )); do
     case $1 in
@@ -130,7 +122,8 @@ gps() {
 
   if [ $isForcePush -ne 0 ]; then
     echo "Invoking 'git push --force-if-includes --force-with-lease " "${args[@]}" "'"
-    read -rp "Are you sure? [y/N]: " key
+    printf '%s' "Are you sure? [y/N]: "
+    read -r key
     [ "$key" != "y" ] && return 1
     git push --force-if-includes --force-with-lease "${args[@]}"
   else
@@ -143,7 +136,7 @@ alias w32gcc='i686-w64-mingw32-gcc -Wall -Wextra -O3 -std=gnu2x -static -s -lm'
 alias w64g++='x86_64-w64-mingw32-g++ -Wall -Wextra -O3 -std=gnu++2a -static -s -lm'
 alias w64gcc='x86_64-w64-mingw32-gcc -Wall -Wextra -O3 -std=gnu2x -static -s -lm'
 
-if [ "$(uname -o)" = "Darwin" ]; then
+if [[ "$OSTYPE" == darwin* ]]; then
   alias ng++='g++-{1..100} -Wall -Wextra -O3 -std=gnu++23 -lm'
   alias ngcc='gcc-{1..100} -Wall -Wextra -O3 -std=gnu2x -lm'
   alias nclang++='clang++ -Wall -Wextra -O3 -std=gnu++2b -arch x86_64 -arch arm64 -lm'
@@ -161,49 +154,56 @@ else
   alias wceg++='arm-mingw32ce-g++ -Wall -Wextra -O3 -std=gnu++2a -march=armv5tej -mcpu=arm926ej-s -static -s -lcommctrl -lcommdlg -lmmtimer -lm'
   alias wcegcc='arm-mingw32ce-gcc -Wall -Wextra -O3 -std=gnu2x -march=armv5tej -mcpu=arm926ej-s -static -s -lcommctrl -lcommdlg -lmmtimer -lm'
 
-  if [ "$(uname -o)" = "Android" ]; then
-    alias pkgi='pkg install'
-    alias pkgr='pkg remove'
-    alias pkga='apt autoremove'
-    alias pkgu='pkg upgrade'
-    alias pkgf='pkg install --fix-broken'
-    alias pkgs='pkg search'
-    alias pkgif='pkg show'
-  elif [ "$(uname -o)" = "FreeBSD" ]; then
-    alias pkgi='sudo pkg install'
-    alias pkgr='sudo pkg remove'
-    alias pkga='sudo pkg autoremove'
-    alias pkgu='sudo pkg upgrade'
-    alias pkgf='sudo pkg check -ad'
-    alias pkgs='pkg search'
-    alias pkgif='pkg info'
-  elif type dnf &> /dev/null; then
-    alias pkgi='sudo dnf install'
-    alias pkgr='sudo dnf remove'
-    alias pkga='sudo dnf autoremove'
-    alias pkgu='sudo dnf upgrade'
-    alias pkgs='dnf search'
-    alias pkgif='dnf info'
-  elif type apt &> /dev/null; then
-    alias pkgi='sudo apt install'
-    alias pkgr='sudo apt remove'
-    alias pkga='sudo apt autoremove'
-    alias pkgu='sudo apt update && sudo apt dist-upgrade'
-    alias pkgf='sudo apt --fix-broken install'
-    alias pkgs='apt search'
-    alias pkgif='apt show'
-  elif type pacman &> /dev/null; then
-    alias pkgi='sudo pacman -S'
-    alias pkgr='sudo pacman -R'
-    alias pkga='sudo pacman -Rs $(pacman -Qdtq)'
-    alias pkgu='sudo pacman -Syyu'
-    alias pkgs='pacman -Ss'
-    alias pkgif='pacman -Si'
-  fi
+  case "$OSTYPE" in
+    linux-android*)
+      alias pkgi='pkg install'
+      alias pkgr='pkg remove'
+      alias pkga='apt autoremove'
+      alias pkgu='pkg upgrade'
+      alias pkgf='pkg install --fix-broken'
+      alias pkgs='pkg search'
+      alias pkgif='pkg show'
+      ;;
+    freebsd*)
+      alias pkgi='sudo pkg install'
+      alias pkgr='sudo pkg remove'
+      alias pkga='sudo pkg autoremove'
+      alias pkgu='sudo pkg upgrade'
+      alias pkgf='sudo pkg check -ad'
+      alias pkgs='pkg search'
+      alias pkgif='pkg info'
+      ;;
+    linux*)
+      if type dnf &> /dev/null; then
+        alias pkgi='sudo dnf install'
+        alias pkgr='sudo dnf remove'
+        alias pkga='sudo dnf autoremove'
+        alias pkgu='sudo dnf upgrade'
+        alias pkgs='dnf search'
+        alias pkgif='dnf info'
+      elif type apt &> /dev/null; then
+        alias pkgi='sudo apt install'
+        alias pkgr='sudo apt remove'
+        alias pkga='sudo apt autoremove'
+        alias pkgu='sudo apt update && sudo apt dist-upgrade'
+        alias pkgf='sudo apt --fix-broken install'
+        alias pkgs='apt search'
+        alias pkgif='apt show'
+      elif type pacman &> /dev/null; then
+        alias pkgi='sudo pacman -S'
+        alias pkgr='sudo pacman -R'
+        alias pkga='sudo pacman -Rs $(pacman -Qdtq)'
+        alias pkgu='sudo pacman -Syyu'
+        alias pkgs='pacman -Ss'
+        alias pkgif='pacman -Si'
+      fi
+      ;;
+  esac
 fi
 
 # make a transparent image opaque (color, src, dest)
 opaque() {
+  local MAGICK
   if which magick &> /dev/null; then
     MAGICK=magick
   elif which convert &> /dev/null; then
@@ -217,6 +217,7 @@ opaque() {
 
 # make an image round (size, margin, src, dest)
 round() {
+  local MAGICK
   if which magick &> /dev/null; then
     MAGICK=magick
   elif which convert &> /dev/null; then
@@ -227,3 +228,32 @@ round() {
   fi
   $MAGICK -size "$1x$1" xc:none -draw "roundrectangle 0,0 $(($1-1)),$(($1-1)) $2,$2" "$3" -compose src-in -composite "$4"
 }
+
+if [ -n "$BASH_VERSION" ]; then
+    shopt -s histappend
+    shopt -s checkwinsize
+    HISTCONTROL=ignoreboth
+    HISTSIZE=10000
+    HISTFILESIZE=10000
+    alias applyrc='. ~/.bashrc'
+
+    if [ -n "$SSH_CONNECTION" ]; then
+      PS1='\n\[$(get_status_color $?)\]$?\[\033[00m\] \[\033[01;32m\]\h\[\033[00m\]:\[\033[01;34m\]$(prompt_pwd)\[\033[00m\]\$ '
+    else
+      PS1='\n\[$(get_status_color $?)\]$?\[\033[00m\] \[\033[01;34m\]$(prompt_pwd)\[\033[00m\]\$ '
+    fi
+elif [ -n "$ZSH_VERSION" ]; then
+    setopt HIST_IGNORE_DUPS
+    setopt HIST_IGNORE_SPACE
+    setopt APPEND_HISTORY
+    setopt PROMPT_SUBST
+    HISTSIZE=10000
+    SAVEHIST=10000
+    alias applyrc='. ~/.zshrc'
+
+    if [ -n "$SSH_CONNECTION" ]; then
+      PS1=$'\n%{$(get_status_color $?)%}%?%{\033[00m%} %{\033[01;32m%}%m%{\033[00m%}:%{\033[01;34m%}$(prompt_pwd)%{\033[00m%}%# '
+    else
+      PS1=$'\n%{$(get_status_color $?)%}%?%{\033[00m%} %{\033[01;34m%}$(prompt_pwd)%{\033[00m%}%# '
+    fi
+fi
